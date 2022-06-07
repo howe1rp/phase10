c7b055362b80103c272e90e71fb88bef24861d39
howe1rp <howe1rp@users.noreply.github.com>
2022-06-07 15:34:13 -0400

    better images

--- client/src/Components/GamePlayer/GamePlayer.jsx
+++ client/src/Components/GamePlayer/GamePlayer.jsx
@@ -3,7 +3,7 @@ import styles from "./GamePlayer.module.css";
 const GamePlayer = (props) => {
   return (
     <div className={styles.player} key={props.name} onClick={props.onClick}>
-      <img src="https://placekitten.com/128/128" alt="cat" />
+      <img src="https://placekitten.com/256/256" alt="cat" />
       <div className={styles.name}>{props.name}</div>
       <div className={styles.score}>{props.score}pts</div>
       <div className={styles.phase}>Phase: {props.currentPhase}</div>
