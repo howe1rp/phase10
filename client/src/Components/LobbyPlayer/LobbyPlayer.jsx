08ca25caf6b7b15df1babd6498575dc8b5cd659f
howe1rp <howe1rp@users.noreply.github.com>
2022-06-07 17:29:21 -0400

    revert

--- client/src/Components/LobbyPlayer/LobbyPlayer.jsx
+++ client/src/Components/LobbyPlayer/LobbyPlayer.jsx
@@ -2,22 +2,9 @@ import { useState, useEffect } from "react";
 import styles from "./LobbyPlayer.module.css";
 
 const LobbyPlayer = (props) => {
-  const [avatar, setAvatar] = useState();
-
-  useEffect(() => {
-    const getAvatarImg = async () => {
-      fetch("https://placeimg.com/160/160/animals", { mode: "no-cors" }).then(
-        (data) => setAvatar(data.blob())
-      );
-    };
-
-    getAvatarImg();
-  }, []);
-
   return (
     <div className={styles["lobby-player"]} key={props.name}>
-      {/* <img src="https://placeimg.com/80/80/animals" alt="cat" /> */}
-      {avatar && <img src={avatar} alt="cat" />}
+      <img src="https://placeimg.com/80/80/animals" alt="animal" />
       <div className={styles["lobby-player-name"]}>{props.name}</div>
     </div>
   );
