7e49d8cb638850d0453f8d4aab2a73cdaf8148c7
howe1rp <howe1rp@users.noreply.github.com>
2022-06-07 16:30:05 -0400

    try to get unique images

--- client/src/Components/LobbyPlayer/LobbyPlayer.jsx
+++ client/src/Components/LobbyPlayer/LobbyPlayer.jsx
@@ -3,14 +3,21 @@ import styles from "./LobbyPlayer.module.css";
 
 const LobbyPlayer = (props) => {
   const [avatar, setAvatar] = useState();
+
   useEffect(() => {
-    setAvatar("https://placeimg.com/160/160/animals");
+    const getAvatarImg = async () => {
+      fetch("https://placeimg.com/160/160/animals").then((data) =>
+        setAvatar(data)
+      );
+    };
+
+    getAvatarImg();
   }, []);
 
   return (
     <div className={styles["lobby-player"]} key={props.name}>
       {/* <img src="https://placeimg.com/80/80/animals" alt="cat" /> */}
-      <img src={avatar} alt="cat" />
+      {avatar && <img src={avatar} alt="cat" />}
       <div className={styles["lobby-player-name"]}>{props.name}</div>
     </div>
   );
